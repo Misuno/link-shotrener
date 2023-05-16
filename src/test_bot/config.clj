c8007fb1f73645ac9b372402ada42b31a01acbd4
Alexander Misuno <alex@misuno.org>
2023-05-16 13:39:28 +0400

    config reading

---
+++ src/test_bot/config.clj
@@ -0,0 +1,11 @@
+(ns test-bot.config
+  (:require [jsonista.core :as j]))
+
+(def config (atom {}))
+
+(def ^:private config-file
+  "resources/config.json")
+
+(defn read-config! []
+  (swap! config (j/read-value (slurp config-file) j/keyword-keys-object-mapper)))
+
