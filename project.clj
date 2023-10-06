99e80d31567b3c1874f412f0a62b292442f849d0
Aliaksandr Misuno <misuno@MBP-Aliaksandr.lan>
2023-10-06 18:12:18 +0400

    newer dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject test-bot "0.1.3"
+(defproject link-shortener "0.1.3"
   :description "telegram bot-operated link shortener"
   :url "http://example.com/FIXME"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure        "1.8.0" ]
-                 [environ                    "1.1.0" ]
-                 [morse                      "0.2.4" ]
-                 [nano-id                    "1.0.0" ]
+  :dependencies [[org.clojure/clojure        "1.11.1"]
+                 [environ                    "1.2.0"]
+                 [morse                      "0.4.3"]
+                 [nano-id                    "1.0.0"]
                  [org.clojure/java.jdbc      "0.7.12"]
                  [mysql/mysql-connector-java "8.0.33"]
                  [ring/ring-core             "1.10.0"]
-                 [ring/ring-jetty-adapter    "1.8.2" ]
-                 [metosin/jsonista           "0.3.7" ]
-                 [clojure.java-time          "1.2.0" ]]
+                 [ring/ring-jetty-adapter    "1.8.2"]
+                 [metosin/jsonista           "0.3.7"]
+                 [clojure.java-time          "1.2.0"]]
 
-  :plugins [[lein-environ "1.1.0"]]
+  :plugins [[lein-environ "1.2.0"]]
 
   :main ^:skip-aot test-bot.core
   :target-path "target/%s"
